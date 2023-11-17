f5cc7aea80af6ccc41c2dc379d6db10a89d1e5a8
Mikhail <mikhailyuwankho@gmail.com>
2023-11-17 13:55:41 +0700

    webhook

--- projects/server/src/database/index.ts
+++ projects/server/src/database/index.ts
@@ -26,7 +26,7 @@ const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
 });
 
 sequelize.authenticate();
-
+// sequelize.sync({ force: true });
 export const DB = {
   User: UserModel(sequelize),
   sequelize,
