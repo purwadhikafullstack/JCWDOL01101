ec36110f187aeb21a3d59ac5178b410056624576
evelioexcellenta <velioexcel@gmail.com>
2024-01-17 11:54:00 +0700

    FORGOT PASSWORD

--- projects/client/src/pages/auth/ForgotPassword.tsx
+++ projects/client/src/pages/auth/ForgotPassword.tsx
@@ -1,113 +1,108 @@
-import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { useSignIn } from "@clerk/clerk-react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { EyeClosedIcon } from "@radix-ui/react-icons";
-import { EyeIcon, Loader } from "lucide-react";
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { Link, Location, useLocation, useNavigate } from "react-router-dom";
-import z from "zod";
+import { useSignIn } from "@clerk/clerk-react";
+import type { NextPage } from "next";
 
-const forgotSchema = z.object({
-  email: z.string().email().min(2),
-})
+const ForgotPassword: NextPage = () => {
+  const [successfulCreation, setSuccessfulCreation] = useState(false);
+  const [complete, setComplete] = useState(false);
+  const [secondFactor, setSecondFactor] = useState(false);
 
-const ForgotPassword = () => {
-  const navigate = useNavigate()
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState<string | null>(null)
+  const { isLoaded, signIn, setActive } = useSignIn();
 
-  const form = useForm<z.infer<typeof forgotSchema>>({
-    resolver: zodResolver(forgotSchema),
-    defaultValues: {
-      email: "",
-    },
-  })
-  
+  if (!isLoaded) {
+    return null;
+  }
+
+  async function create(e: any) {
+    e.preventDefault();
+    const email = e.target[0].value;
+    await signIn
+      ?.create({
+        strategy: "reset_password_email_code",
+        identifier: email,
+      })
+      .then((_) => {
+        setSuccessfulCreation(true);
+      })
+      .catch((err) => console.error("error", err.errors[0].longMessage));
+  }
+
+  async function reset(e: any) {
+    e.preventDefault();
+    const password = e.target[0].value;
+    const code = e.target[1].value;
+    await signIn
+      ?.attemptFirstFactor({
+        strategy: "reset_password_email_code",
+        code,
+        password,
+      })
+      .then((result) => {
+        if (result.status === "needs_second_factor") {
+          setSecondFactor(true);
+        } else if (result.status === "complete") {
+          setActive({ session: result.createdSessionId });
+          setComplete(true);
+        } else {
+          console.log(result);
+        }
+      })
+      .catch((err) => console.error("error", err.errors[0].longMessage));
+  }
+
   return (
-    <main className="flex w-full h-screen">
-      <div className="flex-1 relative hidden lg:flex flex-col items-center justify-center bg-muted h-full">
-        <h3 className="text-center absolute top-0 left-0 pl-8 pt-10  text-3xl text-primary font-bold">
-          当店 | Toten
-        </h3>
-        <img
-          className="w-[600px] mx-auto"
-          src="/ilus/campaign.svg"
-          alt="sign-in ilustration"
-        />
-      </div>
-      <div className="relative rounded-md flex-1 flex items-center justify-center mx-auto p-4">
-        <span className="absolute top-0 left-0 pl-8 pt-12 font-bold text-primary lg:hidden">
-          当店 | Toten
-        </span>
-        <span className="absolute top-0 right-0 pr-8 pt-10 ">
-          <Link
-            to="/login"
-            className={buttonVariants({
-              variant: "ghost",
-              className: "text-primary hover:text-primary/80",
-            })}
-          >
-            Login
-          </Link>
-        </span>
-        <div className="w-full lg:w-[400px]">
-          <img
-            className="mx-auto lg:hidden"
-            src="/ilus/campaign.svg"
-            alt="sign-in ilustration"
-          />
-           <span className="text-center">
-            <h3 className="font-bold text-lg">Forgot Your Password?</h3>
-            <p className="text-sm text-muted-foreground">
-              Enter your email below so we can help you
-            </p>
-          </span>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-8 mt-6"
-            >
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel htmlFor="email">Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        placeholder="name@example.com"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="Obg-[#212121] p-8 rounded shadow-md w-96">
+        <h1 className="'text-4xl text-center font-semibold mb-8">
+          {successfulCreation && !complete ? "New Password" : "Forgot Password"}
+        </h1>
+        <form onSubmit={!successfulCreation ? create : reset}>
+          {!successfulCreation && !complete && (
+            <>
+              <input
+                type="email"
+                placeholder="e.g john@doe.com"
+                className="w-full border border-gray-300 text-black rounded px-3 py-2 "
+                required
               />
-              <Button className="w-full" type="submit">
-                <Loader
-                  className={`${
-                    isLoading ? "animate-spin" : "hidden"
-                  } w-4 h-4 mr-2`}
-                />
-                Sign Up With Email
-              </Button>
-            </form>
-          </Form>
-        </div>
+
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+              >
+                {" "}
+                Submit
+              </button>
+            </>
+          )}
+          {successfulCreation && !complete && (
+            <>
+              <input
+                type="password"
+                className="w-full border border-gray-300 text-black rounded px-3 py-2 "
+                placeholder="New Password"
+                required
+              />
+              <input
+                type="text"
+                className="w-full border border-gray-300 text-black rounded px-3 py-2 "
+                placeholder="Verification Code"
+                required
+              />
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+              >
+                {" "}
+                Submit
+              </button>
+            </>
+          )}
+          {complete && "You successfully changed you password"}
+          {secondFactor && "2FA is required, this UI does not handle that"}
+        </form>
       </div>
-    </main>
+    </div>
   );
 };
 
